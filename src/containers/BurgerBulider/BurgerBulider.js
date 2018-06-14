23ed21623867120a2572120b1038571179a9d3d4
moingeek <gadmoin@gmail.com>
2018-06-14 16:35:24 +0530

    dynamically adding indrigents

--- src/containers/BurgerBulider/BurgerBulider.js
+++ src/containers/BurgerBulider/BurgerBulider.js
@@ -3,6 +3,14 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBulider extends Component {
+    state = {
+        indrigents:{
+            salad :1,
+            bacon: 1,
+            cheese :2,
+            meat :2
+        }
+    }
     render(){
         return (
             <Aux>
