84902bbe5c41e83f413a941516c6a78d741670ce
moingeek <gadmoin@gmail.com>
2018-06-20 17:23:50 +0530

    Improved Performance of application

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React , {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) => {
-    const indrigentSummary = Object.keys(props.indrigents)
-    .map(igKey => {
+
+
+class OrderSummary extends Component{
+    componentWillUpdate(){
+        console.log('[OrderSummary] will update');
+    }
+
+    render(){
+        const indrigentSummary = Object.keys(this.props.indrigents)
+        .map(igKey => {
         return (<li key = {igKey}>
             <span style= {{textTransform:'capitalize'}}> {igKey} </span> : 
-            {props.indrigents[igKey]} 
+            {this.props.indrigents[igKey]} 
             </li>);
     });
-    
-    return(
-        <Aux>
-            <h3> Your Order </h3>
-            <p> A delicious burger with the following Ingredients:</p>
-            <ul>
-                {indrigentSummary}
-            </ul>
-            <p> <strong>Total Price : {props.price.toFixed(2)}</strong></p>
-            <p>Continue to check out? </p>
-            <Button btnType="Danger" clicked={props.purchaseCancled}> CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}> CONTINUE </Button>
-        </Aux>
-    );
-}
+        return(
+            <Aux>
+                <h3> Your Order </h3>
+                <p> A delicious burger with the following Ingredients:</p>
+                <ul>
+                    {indrigentSummary}
+                </ul>
+                <p> <strong>Total Price : {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to check out? </p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancled}> CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}> CONTINUE </Button>
+            </Aux>
+        );
+    }
+} 
 
-export default orderSummary;
+
+export default OrderSummary;
