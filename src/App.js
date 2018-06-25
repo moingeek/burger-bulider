d44e141b28bb528dea80c2d50860f98cf224b815
moingeek <gadmoin@gmail.com>
2018-06-25 17:39:15 +0530

    React Router modules enabled and installed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBulider from './containers/BurgerBulider/BurgerBulider';
 import CheckOut from './containers/Checkout/Checkout';
-
+import {Route,Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBulider />
-          <CheckOut />
+          <Switch>
+              <Route path="/checkout" component={CheckOut} />
+              <Route path="/" exact component={BurgerBulider} />
+          </Switch>
         </Layout>
       </div>
     );
