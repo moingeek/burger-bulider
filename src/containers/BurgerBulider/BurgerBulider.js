9f656bf952584b4cb81ca90a923b0e9e4a25eeac
moingeek <gadmoin@gmail.com>
2018-06-26 17:30:29 +0530

    Contact Data Component

--- src/containers/BurgerBulider/BurgerBulider.js
+++ src/containers/BurgerBulider/BurgerBulider.js
@@ -85,32 +85,13 @@ class BurgerBulider extends Component {
 
     purchaseContinueHandler = () => {
         // alert("You Contiue");
-        // this.setState({loading:true});
-        // const order = {
-        //     ingredients : this.state.indrigents,
-        //     price : this.state.totalPrice,
-        //     customer : {
-        //         name: 'test',
-        //         address :{
-        //             street : 'test',
-        //             zipCode :'400601',
-        //             Country : 'India'
-        //         },
-        //         email : 'test@test.com'
-        //     },
-        //     deliveryMethod : 'fastest'
-        // }
-        // axios.post('/orders.json',order)
-        // .then(response => {
-        //     this.setState({loading:false,purchasing:false});
-        // })
-        // .catch(error => {
-        //     this.setState({loading:false,purchasing:false});
-        // });
         const queryParams = [];
         for(let i in this.state.indrigents){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.indrigents[i]));
         }
+
+        queryParams.push('Price' + this.state.totalPrice);
+
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname :'/checkout',
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,22 +1,27 @@
 import React,{Component} from 'react';
+import {Route} from 'react-router-dom';
+import ContactData from '../Checkout/ContactData/ContactData';
+
+
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 class Checkout extends Component {
     state = {
-        indrigents: {
-            salad :1,
-            meat:1,
-            cheese:1,
-            bacon:1
-        }
+        indrigents: null,
+        price :0
     }
 
-    componentDidMount (){
+    componentWillMount (){
         const query = new URLSearchParams(this.props.location.search);
         const indrigent = {};
+        let price = 0;
         for (let param of query.entries()){
-            indrigent[param[0]] = +param[1];
+            if(param[0] === 'Price'){
+                price = param[1];
+            }else{
+                indrigent[param[0]] = +param[1];
+            }  
         }
-        this.setState({indrigents:indrigent})
+        this.setState({indrigents:indrigent, totalPrice : price})
     }
 
     checkoutCancelled = () =>{
@@ -33,6 +38,10 @@ class Checkout extends Component {
                 <CheckoutSummary indrigents={this.state.indrigents}
                 checkoutCancelled={this.checkoutCancelled}
                 checkoutContinued={this.checkoutContinued}/>
+
+                <Route 
+                path={this.props.match.path +'/contact-data'} 
+                render={()=> (<ContactData indrigents={this.state.indrigents}/>)} />
             </div>
         );
     }
