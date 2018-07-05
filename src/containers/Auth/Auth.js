f7ca8740bd5cff3959b77c98e34321d0eb601691
moingeek <gadmoin@gmail.com>
2018-07-05 18:00:11 +0530

    Redirection Done

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
+import {Redirect} from 'react-router-dom';
 
 class Auth extends Component {
     state = {
@@ -72,6 +73,12 @@ class Auth extends Component {
         return isValid;
     }
 
+    componentDidMount() {
+        if(!this.props.bulidingBurger && this.props.authRedirectPath){
+            this.props.onSetAuthRedirectPath()
+        }
+    }
+
     inputChangedHandler = ( event, controlName ) => {
         const updatedControls = {
             ...this.state.controls,
@@ -129,8 +136,14 @@ class Auth extends Component {
             );
         }
 
+        let authRedirect = null;
+        if(this.props.isAuthenticated){
+            authRedirect = <Redirect to={this.props.authRedirectPath}/>
+        }
+
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -147,13 +160,17 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return{
         loading : state.auth.loading,
-        error : state.auth.error
+        error : state.auth.error,
+        isAuthenticated : state.auth.token !== null,
+        bulidingBurger : state.burgerBuilder.buliding,
+        authRedirectPath : state.auth.authRedirectPath
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) )
+        onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) ),
+        onSetAuthRedirectPath : () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
 
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -6,7 +6,8 @@ const intialState ={
     token:null,
     userId:null,
     error:null,
-    loading:false
+    loading:false,
+    authRedirectPath : '/'
 };
 
 const authStart = (state,action) =>{
@@ -33,6 +34,10 @@ const authLogout = (state,action) =>{
     return updateObject(state,{token:null,userId:null});
 }
 
+const setAuthRedirectPath = (state,action) =>{
+    return updateObject(state,{authRedirectPath :action.path})
+}
+
 
 const authreducer = (state=intialState,action)=>{
     switch(action.type){
@@ -40,6 +45,7 @@ const authreducer = (state=intialState,action)=>{
         case actionTypes.AUTH_SUCCESS: return authSuccess(state,action)
         case actionTypes.AUTH_FAIL: return authFail(state,action)
         case actionTypes.LOGOUT : return authLogout(state,action)
+        case actionTypes.SET_AUTH_REDIRECT_PATH : return setAuthRedirectPath(state,action);
         default:
             return state;
     }
