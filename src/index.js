853f6bdda00c870eb8b6e5c678f12fcc182d7c41
moingeek <gadmoin@gmail.com>
2018-07-06 15:18:29 +0530

    Project Improved

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ import orderReducer from './store/reducers/order';
 import authreducer from './store/reducers/auth';
 
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
